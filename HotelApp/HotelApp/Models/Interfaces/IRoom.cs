bcab708fb90b64e292d986654f37c8421788d01f
Bashar-Owainat <owainatbashar13@gmail>
2023-08-21 11:37:26 +0300

    deleted some comments

--- HotelApp/HotelApp/Models/Interfaces/IRoom.cs
+++ HotelApp/HotelApp/Models/Interfaces/IRoom.cs
@@ -4,13 +4,7 @@ namespace HotelApp.Models.Interfaces
 {
     public interface IRoom : IGenericRepo<Room>
     {
-        //Task<Room> CreateRoom(Room room);
-        //Task<List<Room>> GetRooms();
-
-        //Task<RoomDTO> GetRoom(int id);
-        //Task<Room> UpdateRoom(int id, Room room);
-
-        //Task DeleteRoom(int id);
+       
 
         Task AddAmenityToRoom(int roomId, int amenityId);
         Task RemoveAmentityFromRoom(int roomId, int amenityId);
